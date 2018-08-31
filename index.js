79d233b421bfe6299769b83d0b01e12a28699da6
Indra Putra <indrakoslab@gmail.com>
2018-08-31 16:37:35 +0700

    simple telegram chatbot

---
+++ index.js
@@ -0,0 +1,18 @@
+// process.env.NTBA_FIX_319 = 1;
+
+const Telegram = require('node-telegram-bot-api')
+const dbot = require('dbot-js')
+
+const token = process.env.TELEGRAM_TOKEN
+const mondayCoolBot = new Telegram(token, {polling: true})
+
+mondayCoolBot.on('message', (msg)=> {
+  var chatId = msg.chat.id
+  var message = msg.text.toString()
+
+dbot.get_response(message, (err, res)=> {
+    if(!err){
+        mondayCoolBot.sendMessage(chatId, res)
+    }
+  })
+})
